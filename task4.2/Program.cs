19a633412f0c1d13020ddf142e1706e0a8e4e990
Anastasia <sawiii@mail.ru>
2022-09-01 15:45:25 +0300

    Task 4.2 with mistake DivideByZeroException

---
+++ task4.2/Program.cs
@@ -0,0 +1,28 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму 
+//цифр в числе, которые являются делителями этого числа. 
+//(для получения цифр числа операцию приведения числа к строке не использовать)
+Console.WriteLine("Введите число:");
+int x = Convert.ToInt32(Console.ReadLine());
+int tmp = x;
+int count = 0;
+int last_digit;
+while (tmp > 0)
+{
+    last_digit = tmp % 10;
+    //Console.WriteLine(last_digit);
+    // if (last_digit != 0)
+    //{
+        if (x % last_digit == 0)
+        {
+            count += last_digit;
+            //Console.WriteLine(count);
+        }
+    //}
+    //else
+    //{
+        //continue;
+    //}
+    tmp /= 10;
+    //Console.WriteLine(tmp);
+}
+Console.WriteLine(count);
