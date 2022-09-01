84fa351b83a71d0b4251085506b66bd8ce069e17
Anastasia <sawiii@mail.ru>
2022-09-01 14:50:23 +0300

    Task 4.2 with mistakes

---
+++ task1/task4.2/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму 
+//цифр в числе, которые являются делителями этого числа. 
+//(для получения цифр числа операцию приведения числа к строке не использовать)
+Console.WriteLine("Введите число:");
+int x = Convert.ToInt32(Console.ReadLine());
+int tmp = 0;
+int count = 0;
+int last_digit;
+while (tmp > 0)
+{
+    last_digit = tmp % 10;
+    if (x % last_digit == 0)
+    {
+        count += last_digit;
+    }
+    tmp /= 10;
+}
+Console.WriteLine(count);
