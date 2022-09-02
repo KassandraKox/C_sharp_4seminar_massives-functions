fb2c16934e34c5b3adaea285c7b2f78073e37522
Anastasia <sawiii@mail.ru>
2022-09-02 11:39:47 +0300

    Исправлены обнуление суммы и произведения в цикле, добавлен %

--- task4.3x/Program.cs
+++ task4.3x/Program.cs
@@ -2,10 +2,8 @@
 //Напишите программу, которая заполняет массив на N «интересных» случайных целых чисел. 
 //(Каждый эл-т массива должен быть сгенерирован случайно)
 Random rand = new Random();
-Console.WriteLine("Сколько интересных числе тебе показать? Введи число:");
+Console.WriteLine("Сколько интересных чисел тебе показать? Введи число:");
 int n = Convert.ToInt32(Console.ReadLine()); // число n
-int product = 1;  // произведение цифр
-int total = 0;  // сумма цифр
 int last_digit;  // последняя цифра числа
 int i = 0; // счетчик
 int y; // произведение / сумма
@@ -15,19 +13,23 @@ int [] nums = new int [n];  // создать массив на n- чисел
 while (i < n)
 {
     int x = rand.Next(101); // x - претендент на интересность, ограничен 0-100
-    //Console.WriteLine(x);
+    Console.WriteLine("Претендент: " + x);
     int tmp = x;  // временный x
+    int product = 1;  // произведение цифр
+    int total = 0;  // сумма цифр
     while (tmp > 0)
     {
     last_digit = tmp % 10;
     //Console.WriteLine(last_digit);
     total += last_digit;
-    //Console.WriteLine(total);
+    Console.WriteLine("Сумма: " + total);
     product *= last_digit;
-    //Console.WriteLine(product);
+    Console.WriteLine("Произведение: " + product);
     tmp /= 10;
     }
-    y = product / total;
+
+    y = product % total;
+    Console.WriteLine("Остаток от деления: " + y);
     if ( y == 0)
         {
            nums[i] = x; 
